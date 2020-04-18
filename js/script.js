f070e04a5aebaa3c550d067b1ea91c6340d338df
MdEstiakAhmed <estiak97@gmail.com>
2020-04-18 23:21:13 +0600

    graph of case by date added

--- js/script.js
+++ js/script.js
@@ -11,8 +11,41 @@ fetch('https://api.thevirustracker.com/free-api?countryTotal=BD')
 .then(res => res.json())
 .then(data => {
     setText(data.countrydata[0]);
-    console.log(data.countrydata[0].total_cases);
 })
+
+
+fetch('https://api.thevirustracker.com/free-api?countryTimeline=BD')
+.then(res => res.json())
+.then(data => {
+    var x= Object.keys(data.timelineitems[0]);
+    var value =[];
+    for (let index = 0; index < (Object.keys(data.timelineitems[0]).length)-1; index++) {
+        value.push(data.timelineitems[0][x[index]].total_cases);
+    }
+    console.log(value);
+    
+    showChart(JSON.stringify(Object.keys(data.timelineitems[0])), value)
+})
+
+//chart display
+function showChart(val, res){
+    console.log(JSON.parse(val));
+    console.log(res);
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: JSON.parse(val),
+            datasets: [{
+                label: 'Bangladesh COVID-19 case',
+                borderColor: 'rgb(0, 0, 0)',
+                data: res
+            }]
+        },
+        options: {}
+    });
+}
+
 
 //set text value
 function setText(data){
