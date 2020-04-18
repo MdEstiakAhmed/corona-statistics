7f52fb4ed5b9592340a3f4b1200fa276088bd00c
MdEstiakAhmed <estiak97@gmail.com>
2020-04-18 18:52:15 +0600

    bd today statistic added

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const total_cases = document.getElementById("total-cases");
 const total_death = document.getElementById("total-death");
 const total_recovered = document.getElementById("total-recovered");
 
+const today_cases = document.getElementById("today-cases");
+const today_death = document.getElementById("today-death");
+
 //fetch data
 fetch('https://api.thevirustracker.com/free-api?countryTotal=BD')
 .then(res => res.json())
@@ -16,4 +19,7 @@ function setText(data){
     total_cases.innerHTML = data.total_cases;
     total_death.innerHTML = data.total_deaths;
     total_recovered.innerHTML = data.total_recovered;
+
+    today_cases.innerHTML = data.total_new_cases_today;
+    today_death.innerHTML = data.total_new_deaths_today;
 }
